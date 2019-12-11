163bdc7414573e86577e87a099b593550b0af2c0
Myung <god.godzilla.af@gmail.com>
2019-12-10 22:51:25 -0500

    computer selection generator

---
+++ gameplay.js
@@ -0,0 +1,14 @@
+//Odin Project [Web Develpment 101] [Project: Rock Paper Scissors]
+
+function computerPlay() {
+	var hand = [0, 1, 2];
+	var vhand = hand[Math.floor(Math.random() * hand.length)];
+	return vhand()
+}
+
+
+function playRoung(playerSelection, computerSelection) {
+	player = playerSelection
+	cpu = computerSelection
+	if (player = 1)
+}
