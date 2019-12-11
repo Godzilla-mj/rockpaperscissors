318da1648c9b0895a45e1564210c0e9c109d72ab
Myung <god.godzilla.af@gmail.com>
2019-12-11 00:21:50 -0500

    playRoung() working.

--- gameplay.js
+++ gameplay.js
@@ -1,14 +1,45 @@
 //Odin Project [Web Develpment 101] [Project: Rock Paper Scissors]
 
 function computerPlay() {
-	var hand = [0, 1, 2];
-	var vhand = hand[Math.floor(Math.random() * hand.length)];
-	return vhand()
+	const computerSelection = ['ROCK', 'PAPER', 'SCISSORS'];
+	return computerSelection[Math.floor(Math.random() * computerSelection.length)];
 }
 
 
-function playRoung(playerSelection, computerSelection) {
-	player = playerSelection
-	cpu = computerSelection
-	if (player = 1)
+function playRound(){
+	const ps = prompt("Rock, Paper, or Scissors?");
+	player = ps.toUpperCase()
+	console.log("You chose " + player)
+	cpu = computerPlay();
+	console.log("Computer chose " + cpu)
+	if (player === cpu) {
+		console.log("Draw!")
+	} if (player === "ROCK") {
+		if (cpu === "PAPER") {
+			console.log("You Lose! " + cpu + " beats " + player)
+		} else {
+			console.log("You Win! " + player + " beats " + cpu)
+		}
+	} if (player === "PAPER") {
+		if (cpu === "ROCK"){
+			console.log("You Win! " + player + " beats " + cpu)
+		}
+		else {
+			console.log("You Lose! " + cpu + " beats " + player)
+		}
+	} if (player === "SCISSORS") {
+		if (cpu === "ROCK"){
+			console.log("You Lose! " + cpu + " beats " + player)
+		} else {
+			console.log("You Win! " + player + " beats " + cpu)
+		}
+	}
 }
+
+
+
+
+
+
+
+
