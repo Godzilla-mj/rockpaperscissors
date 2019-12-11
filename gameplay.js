6b1ea2fe5be43b349056d09d2f34b9cb431cb63e
Myung <god.godzilla.af@gmail.com>
2019-12-11 15:55:45 -0500

    -comment added -cleaned up lines

--- gameplay.js
+++ gameplay.js
@@ -3,7 +3,7 @@
 function computerPlay() {
 	const computerSelection = ['ROCK', 'PAPER', 'SCISSORS'];
 	return computerSelection[Math.floor(Math.random() * computerSelection.length)];
-}
+} //computer hand selection
 
 
 function playRound(){
@@ -25,17 +25,17 @@ function playRound(){
 		if (cpu === "ROCK"){playerLose()
 		} else {playerWin()}
 	}
-}
+} //determine winner
 
 function playerWin(){
 	console.log("You Win! " + player + " beats " + cpu)
 	++playerScore
-}
+} //output if player wins round
 
 function playerLose(){
 	console.log("You Lose! " + cpu + " beats " + player)
 	++computerScore
-}
+} //output if computer wins round
 
 function gameEnd(){
 	if (playerScore > computerScore){
@@ -43,7 +43,7 @@ function gameEnd(){
 	}else{
 			console.log("You Lost! Play Again?")
 	}
-}
+} //output for winner for game
 
 
 function game(){
@@ -52,13 +52,14 @@ function game(){
 	console.log("Best out of 5 rounds!")
 	while ((playerScore + computerScore) < 5){
 		playRound()
-		console.log("Player Score: " + playerScore + "\n Computer Score " + computerScore)
+		console.log("Player Score: " + playerScore + "\nComputer Score " 
+				+ computerScore)
 		if (playerScore >= 3 || computerScore >= 3){
 			gameEnd()
 			break;
-	}
+	} //first to 3 to win
 }
-}
+} //best out of 5 round game function
 
 
 
