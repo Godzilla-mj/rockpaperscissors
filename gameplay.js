312918bc345bd3898b93c52d767bb2e6fb9653a0
Myung <god.godzilla.af@gmail.com>
2019-12-11 15:09:22 -0500

    game() added. -fully functional

--- gameplay.js
+++ gameplay.js
@@ -14,29 +14,51 @@ function playRound(){
 	console.log("Computer chose " + cpu)
 	if (player === cpu) {
 		console.log("Draw!")
+		return
 	} if (player === "ROCK") {
 		if (cpu === "PAPER") {
 			console.log("You Lose! " + cpu + " beats " + player)
+			++computerScore
 		} else {
 			console.log("You Win! " + player + " beats " + cpu)
+			++playerScore
 		}
 	} if (player === "PAPER") {
 		if (cpu === "ROCK"){
 			console.log("You Win! " + player + " beats " + cpu)
+			++playerScore
 		}
 		else {
 			console.log("You Lose! " + cpu + " beats " + player)
+			++computerScore
 		}
 	} if (player === "SCISSORS") {
 		if (cpu === "ROCK"){
 			console.log("You Lose! " + cpu + " beats " + player)
+			++computerScore
 		} else {
 			console.log("You Win! " + player + " beats " + cpu)
+			++playerScore
 		}
 	}
 }
 
+playerScore = 0
+computerScore = 0
 
+function game(){
+	while ((playerScore + computerScore) < 5){
+		playRound()
+		console.log("Player Score: " + playerScore + "\n Computer Score " + computerScore)
+	}
+	if ((playerScore + computerScore) === 5){
+		if (playerScore > computerScore){
+			console.log("You Won! Play Again?")
+		}else{
+			console.log("You Lost! Play Again?")
+		}
+	}
+}
 
 
 
